b08fb99128644614c5992e7ccca42ad8e26edeff
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-06-05 18:56:21 +0200

    added EF Models

---
+++ MonkFocusModels/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkFocusModels
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string Status { get; set; }
+        public int Priority { get; set; }
+        public virtual Users User { get; set; }
+    }
+}
