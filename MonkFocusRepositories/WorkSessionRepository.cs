83f1c10e867ae4e5ae8e053f9f7207ed035971d6
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-06-28 22:10:20 +0200

    xml notation

--- MonkFocusRepositories/WorkSessionRepository.cs
+++ MonkFocusRepositories/WorkSessionRepository.cs
@@ -1,71 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MonkFocusDataAccess;
+﻿using MonkFocusDataAccess;
 using MonkFocusModels;
 using MonkFocusRepositories.Interfaces;
 
-namespace MonkFocusRepositories
+namespace MonkFocusRepositories;
+
+/// <summary>
+///     This method is used to perform CRUD operations on the WorkSession table in the database.
+/// </summary>
+public class WorkSessionRepository : IWorkSessionRepository
 {
-    /// <summary>
-    /// This method is used to perform CRUD operations on the WorkSession table in the database.
-    /// </summary>
-    public class WorkSessionRepository : IWorkSessionRepository
+    #region DI Fields
+
+    private readonly MonkFocusDbContext _context;
+
+    #endregion
+
+    public WorkSessionRepository(MonkFocusDbContext context)
     {
-        #region DI Fields
-        private readonly MonkFocusDbContext _context;
-        #endregion
+        _context = context;
+    }
 
-        public WorkSessionRepository(MonkFocusDbContext context)
+    /// <summary>
+    ///     This method adds a new work session to the database.
+    /// </summary>
+    /// <param name="workSession">given work session</param>
+    /// <exception cref="System.ArgumentNullException">Work session you've provided was null</exception>
+    public void AddWorkSession(WorkSession workSession)
+    {
+        if (workSession is null) throw new ArgumentNullException(nameof(workSession));
+
+        _context.WorkSessions.Add(workSession);
+        _context.SaveChanges();
+    }
+
+    /// <summary>
+    ///     This method deletes a new work session to the database.
+    /// </summary>
+    /// <param name="workSession">given work session</param>
+    public void DeleteWorkSessionById(int workSessionId)
+    {
+        var WorkSessionToDelete = _context.WorkSessions
+            .FirstOrDefault(w => w.WorkSessionId == workSessionId);
+
+        if (WorkSessionToDelete != null)
         {
-            _context = context;
-        }
-
-        /// <summary>
-        /// This method adds a new work session to the database.
-        /// </summary>
-        /// <param name="workSession">given work session</param>
-        /// <exception cref="System.ArgumentNullException">Work session you've provided was null</exception>
-        public void AddWorkSession(WorkSession workSession)
-        {
-            if(workSession is null) throw new System.ArgumentNullException(nameof(workSession));
-
-            _context.WorkSessions.Add(workSession);
+            _context.WorkSessions.Remove(WorkSessionToDelete);
             _context.SaveChanges();
         }
+    }
 
-        /// <summary>
-        /// This method deletes a new work session to the database.
-        /// </summary>
-        /// <param name="workSession">given work session</param>
-        public void DeleteWorkSessionById(int workSessionId)
-        {
-            var WorkSessionToDelete = _context.WorkSessions
-                .FirstOrDefault(w => w.WorkSessionId == workSessionId);
+    /// <summary>
+    ///     This method gets all work sessions from the database.
+    /// </summary>
+    /// <param name="workSession">given work session</param>
+    public IEnumerable<WorkSession> GetAllWorkSessionsForUser(int userId)
+    {
+        var usersWorkSessions = _context.WorkSessions
+            .Where(w => w.UserId == userId)
+            .ToList();
 
-            if (WorkSessionToDelete != null)
-            {
-                _context.WorkSessions.Remove(WorkSessionToDelete);
-                _context.SaveChanges();
-            }
-        }
+        if (usersWorkSessions is null) return null;
 
-        /// <summary>
-        /// This method gets all work sessions from the database.
-        /// </summary>
-        /// <param name="workSession">given work session</param>
-        public IEnumerable<WorkSession> GetAllWorkSessionsForUser(int userId)
-        {
-            var usersWorkSessions = _context.WorkSessions
-                .Where(w => w.UserId == userId)
-                .ToList();
-
-            if (usersWorkSessions is null) return null;
-
-            return usersWorkSessions;
-        }
-
+        return usersWorkSessions;
     }
 }
